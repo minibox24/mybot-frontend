d314ff65b8b1e05f8002f2413c74323870b9ff11
minibox24 <minibox724@gmail.com>
2021-07-27 19:18:32 +0900

    ✨ 메인 페이지

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;900&display=swap");
+
+body {
+  margin: 0;
+  font-family: "Noto Sans KR", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  height: 100vh;
+  background: #283033;
+  color: white;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="info">
+      <img alt="Avatar" src="../assets/minibot.png" class="avatar" />
+      <span class="title">미니봇</span>
+    </div>
+    <div class="btns">
+      <a class="btn" href="https://minibox.xyz/invite" target="blank"
+        >초대하기</a
+      >
+      <a class="btn" href="https://discord.gg/pbd2xXJ" target="blank"
+        >서포트 서버</a
+      >
+      <a class="btn" href="https://minibox.xyz/vote" target="blank"
+        >하트 누르기</a
+      >
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style scoped>
+.home {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+.info {
+  display: flex;
+  align-items: center;
+}
+
+.avatar {
+  width: 300px;
+  border-radius: 50px;
+}
+
+.title {
+  font-weight: bold;
+  font-size: 10rem;
+  margin-left: 1rem;
+}
+
+.btns {
+  display: flex;
+  margin-top: 2rem;
+}
+
+.btn {
+  text-decoration: none;
+  color: white;
+  background: #5865f2;
+  min-width: 100px;
+  text-align: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+  margin: 0.25rem;
+  transition: 0.17s;
+}
+
+.btn:hover {
+  background: #4752c4;
+}
+
+@media (max-width: 900px) {
+  .info {
+    flex-direction: column;
+  }
+
+  .avatar {
+    width: 200px;
+    border-radius: 25px;
+  }
+
+  .title {
+    font-size: 4rem;
+    margin-left: 0;
+  }
+
+  .btns {
+    flex-direction: column;
+  }
+}
+</style>

1e5c2e4daed0e95b3cb9d84aaf5e73a9fb065233
minibox24 <minibox724@gmail.com>
2021-07-28 16:52:28 +0900

    ✨ 디스코드 메시지 컴포넌트

---
+++ src/components/Message.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="message">
+    <img class="avatar" alt="avatar" :src="avatar" @error="defaultImage" />
+    <div class="contents">
+      <div class="header">
+        <span class="name">{{ name ?? "마이봇" }}</span>
+        <span class="bot">봇</span>
+        <span class="time">오늘 오전 12:00</span>
+      </div>
+      <span class="content">{{ content ?? "안녕하세요!" }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    avatar: String,
+    content: String,
+  },
+  methods: {
+    defaultImage(e) {
+      e.target.src = "https://cdn.discordapp.com/embed/avatars/0.png";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  background: #36393f;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+}
+
+.contents {
+  margin-left: 0.5rem;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+}
+
+.name {
+  max-width: 300px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.bot {
+  font-size: 0.625rem;
+  background: #5865f2;
+  margin-left: 0.25rem;
+  height: 0.9375rem;
+  padding: 0 0.275rem;
+  margin-top: 0.075em;
+  border-radius: 0.1875rem;
+}
+
+.time {
+  color: #72767d;
+  font-size: 0.75rem;
+  margin-left: 0.3rem;
+}
+
+.content {
+  margin-top: -10px;
+  font-weight: lighter;
+}
+</style>

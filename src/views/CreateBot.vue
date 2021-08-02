118020cb91b704fee1ea83b3d3f4f5debe77c859
minibox24 <minibox724@gmail.com>
2021-08-02 13:46:35 +0900

    ✨ 봇 만들기 페이지 API 연결

--- src/views/CreateBot.vue
+++ src/views/CreateBot.vue
@@ -3,27 +3,45 @@
     <div class="header">
       <span class="title">봇 만들기</span>
       <div class="user">
-        <span class="user-name">Minibox#3332</span>
-        <img
-          class="user-avatar"
-          alt="avatar"
-          src="https://cdn.discordapp.com/avatars/310247242546151434/3bc87392aa69fe30a659f72151062458.png"
-        />
+        <span class="user-name">{{ user.name }}</span>
+        <img class="user-avatar" alt="avatar" :src="user.avatar" />
       </div>
     </div>
-    <div class="inputs">
-      <Input :width="20" label="아이디" v-model="id" />
-      <Input :width="20" label="이름" v-model="name" />
-      <Input :width="20" label="프리픽스" />
-      <Input :width="20" label="프로필 사진 URL" v-model="avatar" />
+    <form class="inputs" v-on:submit.prevent="submit">
+      <Input
+        :width="20"
+        label="이름"
+        v-model="name"
+        pattern="^.{1,65}$"
+        title="봇의 이름은 최소 1자, 최대 65자입니다."
+        :required="true"
+      />
+      <Input
+        :width="20"
+        label="프리픽스"
+        v-model="prefix"
+        pattern="^.{1,20}$"
+        title="봇의 프리픽스는 최소 1자, 최대 20자입니다."
+        :required="true"
+      />
+      <Input
+        :width="20"
+        label="프로필 사진 URL"
+        v-model="avatar"
+        pattern="^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$"
+        title="이미지 URL 형식만 가능합니다."
+        :required="false"
+      />
+
+      <input ref="submit" type="submit" style="display: none" />
 
       <div class="preview">
         <span>미리보기</span>
         <Message style="margin-top: 0.5rem" :name="name" :avatar="avatar" />
       </div>
-    </div>
+    </form>
     <div class="button-container">
-      <span class="button">만들기</span>
+      <span class="button" @click="submitButton">만들기</span>
     </div>
   </div>
 </template>
@@ -31,16 +49,56 @@
 <script>
 import Input from "../components/Input.vue";
 import Message from "../components/Message.vue";
+import axios from "axios";
 
 export default {
   components: { Input, Message },
   data() {
     return {
-      id: "",
       name: null,
-      prefix: "",
-      avatar: "",
+      prefix: null,
+      avatar: null,
+      user: {
+        name: "",
+        avatar: "https://cdn.discordapp.com/embed/avatars/0.png",
+      },
     };
+  },
+  async mounted() {
+    const token = this.$route.query.token;
+
+    if (!token) {
+      return this.$router.push("/");
+    }
+
+    const { status, data } = await axios.get(`/createBot?token=${token}`);
+
+    if (status !== 200) {
+      return this.$router.push("/");
+    }
+
+    this.user.name = data.userName;
+    this.user.avatar = data.userAvatar;
+  },
+  methods: {
+    async submit() {
+      const { status } = await axios
+        .post(`/createBot?token=${this.$route.query.token}`, {
+          name: this.name,
+          prefix: this.prefix,
+          avatar: this.avatar,
+        })
+        .catch(() => {
+          alert("알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+        });
+
+      if (status === 200) {
+        this.$router.push("/done");
+      }
+    },
+    submitButton() {
+      this.$refs.submit.click();
+    },
   },
 };
 </script>

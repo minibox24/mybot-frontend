cbced7c5dace9a0bd818205339a7da9eac89c3c0
minibox24 <minibox724@gmail.com>
2021-08-02 14:42:15 +0900

    ✨ base url 변경

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8081";
+axios.defaults.baseURL = "https://api.minibox.xyz";
 
 createApp(App).use(router).mount("#app");
